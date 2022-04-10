7298af7ce8c940f9aba8a112594d3272134b654d
dorsi-alt <83610525+dorsi-alt@users.noreply.github.com>
2022-04-09 20:50:06 -0400

    removed dropdown

--- packages/react-app/src/components/Header/TopHeader.js
+++ packages/react-app/src/components/Header/TopHeader.js
@@ -50,11 +50,6 @@ export const TopHeader = () => {
           <TryNowBtn>Try Now</TryNowBtn>
         </WhaleModeBorder>
       </WhaleModeDiv>
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
       <WalletButton />
     </Header>    
   );
